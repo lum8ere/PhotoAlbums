6718b4f838b86ee93761f8ceb8226efc2a1eeff0
aleksandr.zaburdaev <aleksandr.zaburdaev@inthemelab.com>
2023-05-09 16:01:47 +0300

    added loadingSpinner to imageModel

--- frontend/src/components/ui/Modals/ImageModels.tsx
+++ frontend/src/components/ui/Modals/ImageModels.tsx
@@ -1,26 +1,33 @@
+import { useState, useEffect } from 'react';
 import { Modal, Image } from 'react-bootstrap';
 import { Photo } from '../../../types/photos';
+import LoadingSpinner from '../Spinner/Spinner';
 
-const ImageModal = ({ title, thumbnailUrl, show, onHide}: Photo & any) => {
+const ImageModal = ({ title, thumbnailUrl, show, onHide }: Photo & any) => {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, [thumbnailUrl, title]);
+
   return (
     <Modal
       show={show}
       onHide={onHide}
-      s
       size="lg"
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
-            {title}
+          {title}
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Image src={thumbnailUrl} rounded />
+        {loading ? <LoadingSpinner /> : <Image src={thumbnailUrl} rounded />}
       </Modal.Body>
     </Modal>
   );
-}
+};
 
 export default ImageModal;
